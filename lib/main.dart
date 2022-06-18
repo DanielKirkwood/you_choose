7539aa8d009c135f43d030fa6cf9c9eb0f7e9f04
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-06-18 19:59:48 +0100

    [Enhancement]: Added firebase auth to project.
    Also added emulator which is run used in debug mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io' show Platform;
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,7 @@ Future _connectToEmulator() async {
   final host = Platform.isAndroid ? '10.0.2.2' : 'localhost';
 
   const firestorePort = 8080;
+  const authPort = 9099;
 
   // Just to make sure we're running locally
   if (kDebugMode) {
@@ -21,6 +23,7 @@ Future _connectToEmulator() async {
   }
 
   FirebaseFirestore.instance.useFirestoreEmulator(host, firestorePort);
+  await FirebaseAuth.instance.useAuthEmulator(host, authPort);
 }
 
 Future<void> main() async {
