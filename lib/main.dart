8b5fbfba3f4be76a53d57238c42b220fc8944db0
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-10-06 16:02:55 +0100

    moved emulator code to fn

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:you_choose/src/app.dart';
@@ -10,6 +10,25 @@ import 'package:you_choose/src/bloc/bloc_observer.dart';
 import 'package:you_choose/src/util/logger/logger.dart';
 
 import 'firebase_options.dart';
+
+Future<void> _setupEmulators() async {
+  const firestorePort = 8080;
+  const authPort = 9099;
+  const storagePort = 9199;
+
+  // [Firestore | localhost:8080]
+  FirebaseFirestore.instance.settings = const Settings(
+    host: 'localhost:$firestorePort',
+    sslEnabled: false,
+    persistenceEnabled: false,
+  );
+
+  // [Authentication | localhost:9099]
+  await FirebaseAuth.instance.useAuthEmulator('localhost', authPort);
+
+  // [Storage | localhost:9199]
+  await FirebaseStorage.instance.useStorageEmulator('localhost', storagePort);
+}
 
 Future<void> main() async {
   var logger = getLogger('main');
@@ -28,23 +47,11 @@ Future<void> main() async {
   if (!isProduction) {
     logger.d('Connecting to firebase emulators');
 
-    const firestorePort = 8080;
-    const authPort = 9099;
-
-    // [Firestore | localhost:8080]
-    FirebaseFirestore.instance.settings = const Settings(
-      host: 'localhost:$firestorePort',
-      sslEnabled: false,
-      persistenceEnabled: false,
-    );
-
-    // [Authentication | localhost:9099]
-    await FirebaseAuth.instance.useAuthEmulator('localhost', authPort);
+    await _setupEmulators();
   }
 
   HydratedBlocOverrides.runZoned(
     () => runApp(const MyApp()),
     storage: storage,
   );
-
 }
