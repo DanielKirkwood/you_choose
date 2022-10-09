76551864ed8570c17ea73248795c3a404bf95129
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-10-09 16:30:27 +0100

    added doc comments to group_state

--- lib/src/group/cubit/group_state.dart
+++ lib/src/group/cubit/group_state.dart
@@ -1,22 +1,42 @@
 part of 'group_cubit.dart';
 
-enum GroupStatus { initial, loading, success, failure }
+/// enum representing the states which the [GroupCubit] can be in.
+///
+/// [initial] - is the base state.
+///
+/// [loading] - intermediate state used while [GroupCubit] interacts with backend.
+///
+/// [success] - action carried out successfully and state has been updated with changes.
+///
+/// [failure] - action has failed.
+///
+/// [adding] - new group is being added to groups.
+enum GroupStatus { initial, loading, success, failure, adding }
 
+/// {@template groupState}
+/// A single state used to hold state about groups.
+///
+/// Contains the [status] of the state and the list of [Group]s for frontend.
+/// {@endtemplate}
 class GroupState extends Equatable {
-  final List<Group> groups;
-  final GroupStatus status;
-
+  /// {@macro groupState}
   const GroupState(
       {this.groups = const <Group>[], this.status = GroupStatus.initial});
 
-  @override
-  List<Object> get props => [groups, status];
+  /// the list of groups.
+  final List<Group> groups;
 
-  GroupState copyWith({
-    List<Group>? groups,
-    GroupStatus? status
-  }) {
+  /// the status of the state.
+  final GroupStatus status;
+
+  /// Convenience copyWith method to update fields
+  ///
+  /// {@macro groupState}
+  GroupState copyWith({List<Group>? groups, GroupStatus? status}) {
     return GroupState(
         groups: groups ?? this.groups, status: status ?? this.status);
   }
+
+  @override
+  List<Object> get props => [groups, status];
 }
