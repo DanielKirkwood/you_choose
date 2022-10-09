fbabad8239132c7eff9f85bf538621c66f260e43
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-10-09 11:10:46 +0100

    added status to cubits

--- lib/src/profile/cubit/profile_state.dart
+++ lib/src/profile/cubit/profile_state.dart
@@ -1,8 +1,12 @@
 part of 'profile_cubit.dart';
 
+enum ProfileStatus { initial, loading, success, failure }
+
 class ProfileState extends Equatable {
-  const ProfileState();
+  const ProfileState({this.status = ProfileStatus.initial});
+
+  final ProfileStatus status;
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [status];
 }
