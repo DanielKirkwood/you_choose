4739142fde83065c02450b8a9728050676dee414
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-10-10 19:41:12 +0100

    group cubit updated to add group

--- lib/src/group/cubit/group_state.dart
+++ lib/src/group/cubit/group_state.dart
@@ -21,7 +21,12 @@ enum GroupStatus { initial, loading, success, failure, adding }
 class GroupState extends Equatable {
   /// {@macro groupState}
   const GroupState(
-      {this.groups = const <Group>[], this.status = GroupStatus.initial});
+      {this.groups = const <Group>[],
+      this.status = GroupStatus.initial,
+      this.name = const GroupName.pure(),
+      this.members = const GroupMembers.pure(),
+      this.formStatus = FormzStatus.pure,
+      this.errorMessage});
 
   /// the list of groups.
   final List<Group> groups;
@@ -29,14 +34,37 @@ class GroupState extends Equatable {
   /// the status of the state.
   final GroupStatus status;
 
+  /// the new group name.
+  final GroupName name;
+
+  /// the new group members.
+  final GroupMembers members;
+
+  final FormzStatus formStatus;
+
+  final String? errorMessage;
+
+
   /// Convenience copyWith method to update fields
   ///
   /// {@macro groupState}
-  GroupState copyWith({List<Group>? groups, GroupStatus? status}) {
+  GroupState copyWith({
+    List<Group>? groups,
+    GroupStatus? status,
+    GroupName? name,
+    GroupMembers? members,
+    FormzStatus? formStatus,
+    String? errorMessage,
+  }) {
     return GroupState(
-        groups: groups ?? this.groups, status: status ?? this.status);
+        groups: groups ?? this.groups,
+        status: status ?? this.status,
+        name: name ?? this.name,
+        members: members ?? this.members,
+        formStatus: formStatus ?? this.formStatus,
+        errorMessage: errorMessage ?? this.errorMessage);
   }
 
   @override
-  List<Object> get props => [groups, status];
+  List<Object> get props => [groups, status, name, members];
 }
