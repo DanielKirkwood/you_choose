479aae670d2f500223926b0ef3d0904ccf63dbdc
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-10-18 20:13:34 +0100

    profile displays friends

--- lib/src/profile/view/profile_page.dart
+++ lib/src/profile/view/profile_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:you_choose/src/app/app.dart';
 import 'package:you_choose/src/data/data.dart';
+import 'package:you_choose/src/friends/cubit/friends_cubit.dart';
 import 'package:you_choose/src/profile/profile.dart';
+import 'package:you_choose/src/repositories/database/firestore_repository.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -11,73 +13,117 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    UserModel user = context.select((AppBloc bloc) => bloc.state.user);
     return Scaffold(
         appBar: buildAppBar(context: context, hasBackButton: false),
-        body: Stack(children: [
-          ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            physics: const BouncingScrollPhysics(),
-            children: [
-              AvatarProfile(
-                uid: user.uid,
-                useDefault: user.useDefaultProfileImage,
-                onClicked: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) => const EditProfilePage()),
-                  );
-                },
-              ),
-              const SizedBox(height: 24),
-              buildName(user),
-              const SizedBox(height: 24),
-              buildFriends(user),
-              const SizedBox(height: 24),
-              Center(
-              child: TextButton(
-                  onPressed: () =>
-                      context.read<AppBloc>().add(AppLogoutRequested()),
-                  child: const Text(
-                    'Sign Out',
-                    style: TextStyle(color: Colors.red),
-                  )),
-            ),
-            ],
-          ),
+        body: BlocProvider(
+          create: (context) => FriendsCubit(FirestoreRepository()),
+          child: const ProfileView(),
+        ));
+  }
+}
 
-        ]));
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    UserModel user = context.select((AppBloc bloc) => bloc.state.user);
+
+    return ListView(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      physics: const BouncingScrollPhysics(),
+      children: [
+        AvatarProfile(
+          uid: user.uid,
+          useDefault: user.useDefaultProfileImage,
+          onClicked: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const EditProfilePage()),
+            );
+          },
+        ),
+        const SizedBox(height: 24),
+        BuildName(
+          username: user.username,
+          email: user.email,
+        ),
+        const SizedBox(height: 24),
+        Center(
+          child: TextButton(
+              onPressed: () =>
+                  context.read<AppBloc>().add(AppLogoutRequested()),
+              child: const Text(
+                'Sign Out',
+                style: TextStyle(color: Colors.red),
+              )),
+        ),
+        const SizedBox(height: 24),
+        BuildFriends(
+          friends: user.friends,
+        )
+      ],
+    );
+  }
+}
+
+class BuildName extends StatelessWidget {
+  const BuildName({super.key, required this.username, required this.email});
+
+  final String username;
+
+  final String email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          username,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          email,
+          style: const TextStyle(color: Colors.grey),
+        )
+      ],
+    );
+  }
+}
+
+class BuildFriends extends StatelessWidget {
+  const BuildFriends({super.key, required this.friends});
+
+  final Map<String, dynamic> friends;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          'Friends',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 16),
+        ...friends.entries.map((friend) {
+          return ListTile(
+              onTap: () {},
+              leading: const CircleAvatar(backgroundColor: Colors.black),
+              title: Text(friend.value['username']),
+              trailing: const Icon(Icons.arrow_forward_ios));
+        }).toList()
+      ],
+    );
   }
 
-  Widget buildName(UserModel user) => Column(
-        children: [
-          Text(
-            user.username,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            user.email,
-            style: const TextStyle(color: Colors.grey),
-          )
-        ],
-      );
-
-  Widget buildFriends(UserModel user) => Container(
-        padding: const EdgeInsets.symmetric(horizontal: 48),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text(
-              'Friends',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Test',
-              style: TextStyle(fontSize: 16, height: 1.4),
-            ),
-          ],
-        ),
-      );
+  buildListTile() {
+    friends.entries.map((friend) {
+      return ListTile(
+          onTap: () {},
+          leading: const CircleAvatar(backgroundColor: Colors.black),
+          title: Text(friend.value.username),
+          trailing: const Icon(Icons.arrow_forward_ios));
+    });
+  }
 }
